1c5d088d707ef72753ef235701c3bc8a6af7aa9a
Wenbing.Wang <wenbing.wang@rokid.com>
2019-09-19 17:39:05 +0800

    cmake call shell script example

---
+++ shell.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#echo "start to call shell script"
+current_path=`pwd`
+#echo "current_path: ${current_path}"
+#content=`cat args.txt`
+cat ../args.txt
+#echo "content: ${content}"
+#echo "stop to call shell script"
